d9c9f3b825acd2ab2f8cb5b091614da8ecdccee8
Veikko Laine <veikko.laine@gmail.com>
2023-08-28 15:41:07 +0300

    First commit.

---
+++ server/utils/connectMongoose.js
@@ -0,0 +1,17 @@
+ const mongoose = require('mongoose')
+
+const { mongoUri } = require('./config')
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+}
+const connectMongoose = () => {
+    mongoose.connect(mongoUri, options)
+            .then(() => console.log('connected to databases', mongoUri))
+            .catch(err => console.log('connecting to MongoDB failed', err))
+}
+
+mongoose.set('strictQuery', true)
+
+module.exports = connectMongoose
