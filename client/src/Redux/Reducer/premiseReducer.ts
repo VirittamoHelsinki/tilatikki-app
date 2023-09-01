828cfe83ed4179885f2ac0d65e44b7a47870d13e
Alexei Golovanov <goloale@digitalentshelsinki.internal>
2023-09-01 12:10:48 +0300

    removed testComponent cleaned up some code, added new commets for premiseReducer

--- client/src/Redux/Reducer/premiseReducer.ts
+++ client/src/Redux/Reducer/premiseReducer.ts
@@ -3,28 +3,27 @@ import { Reducer, Dispatch } from "redux";
 import { initialState, initialStateProps } from "./initialState"; // for future when initalState and props are moved to its own file
 
 
-// All data here is dummy for testing reducer and actions it shows the structure an how to build reducers
 // start from ActionTypes, Action interface, Action creator's and then reducer
 
 // Action types
 // This defines the different types of actions that can be dispatched to the reducer.
 enum ActionType {
-    UPDATE_COUNTER = "UPDATE_COUNTER"
+    UPDATE_COUNTER = "UPDATE_COUNTER" // example dummy data 
 }
 
 // Action interface
 // This defines the interface for all actions that can be dispatched to the reducer.
 interface UpdateCounterAction {
-    type: typeof ActionType.UPDATE_COUNTER;
+    type: typeof ActionType.UPDATE_COUNTER; //example dummy data
 }
 
 // Collection of all Action interface types
 export type Action =
-|UpdateCounterAction;
+|UpdateCounterAction; // example dummy data
 
 // Action creators
 // These functions create actions that can be dispatched to the reducer.
-export const updateCounter = () => {
+export const updateCounter = () => { // example dummy Action creator
     return async (dispatch: Dispatch<UpdateCounterAction>) => {
         console.log("updateCounter active");
         dispatch({ type: ActionType.UPDATE_COUNTER });
@@ -35,7 +34,7 @@ export const updateCounter = () => {
 // Main reducer
 // This is the main reducer that updates the state of the reducer based on the actions that are dispatched to it.
 const premiseReducer: Reducer<initialStateProps> = (state = initialState,action:Action) => {
-    switch (action.type) {
+    switch (action.type) { // example dummy data
       case ActionType.UPDATE_COUNTER:
         return {
           ...state,
