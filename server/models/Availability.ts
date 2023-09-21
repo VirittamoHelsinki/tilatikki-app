4601a6ce4bce17acba51474eaf5b2b00a17afa2f
Veikko Laine <veikko.laine@gmail.com>
2023-09-21 11:51:17 +0300

    Refactored model interfaces and their exports so that the correct model interface can be used outside the model file. Also added some typeguards that can be used to solve typescript typedefinitions when populating fields for models.

--- server/models/Availability.ts
+++ server/models/Availability.ts
@@ -1,8 +1,7 @@
 import { Document, Schema, Model, model } from 'mongoose';
 
-// Use this or remove?
-export interface LAvailability {
-  _id: Schema.Types.ObjectId;
+// For internal model definition only.
+interface SchemaAvailability {
   creator: Schema.Types.ObjectId;
   startdate: Date;
   enddate: Date;
@@ -11,25 +10,34 @@ export interface LAvailability {
   reservations: Schema.Types.ObjectId[];
 }
 
-export interface IAvailability {
-  //id: Schema.Types.ObjectId;
-  creator: Schema.Types.ObjectId;
-  startdate: Date;
-  enddate: Date;
-  premise: Schema.Types.ObjectId;
-  space: Schema.Types.ObjectId;
-  reservations: Schema.Types.ObjectId[];
+// Use this interface for objects of this type. (adds _id field)
+export interface IAvailability extends SchemaAvailability, Document {}
+
+// Typeguard to check if the value is of type IAvailability.
+export function isAvailability(value: any): value is IAvailability {
+  return (
+    value &&
+    value._id instanceof Schema.Types.ObjectId &&
+    value.startdate instanceof Date &&
+    value.enddate instanceof Date &&
+    value.creator instanceof Schema.Types.ObjectId &&
+    value.premise instanceof Schema.Types.ObjectId &&
+    value.space instanceof Schema.Types.ObjectId &&
+    value.reservations.every((element:any) => element instanceof Schema.Types.ObjectId)
+  )
+};
+
+// Typeguard to check if the value is of type IAvailability[].
+export function isAvailabilityList(value: any): value is IAvailability[] {
+  return (
+    Array.isArray(value) &&
+    value.every(element => element instanceof Availability)
+  );
 }
 
-interface IAvailabilityModel extends IAvailability, Document {}
-
-const availabilitySchema = new Schema<IAvailability>({
+const availabilitySchema = new Schema<SchemaAvailability>({
   // Creator is only required for availabilities created by users.
   // Availabilities that might be added from APIs dont need a creator.
-  // id: {
-  //   type: Schema.Types.ObjectId,
-  //   required: true
-  // },
   creator: {
     type: Schema.Types.ObjectId,
     ref: 'User',
@@ -56,6 +64,6 @@ const availabilitySchema = new Schema<IAvailability>({
   }]
 });
 
-const Availability: Model<IAvailabilityModel> = model<IAvailabilityModel>('Availability', availabilitySchema);
+const Availability: Model<IAvailability> = model<IAvailability>('Availability', availabilitySchema);
 
 export default Availability;
