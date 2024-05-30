c7188792a15729e3e802e9c0c0649748f680d881
Lagical <markuz.rika@gmail.com>
2024-05-30 10:25:11 +0300

    update route

--- server/routes/users.js
+++ server/routes/users.js
@@ -73,4 +73,31 @@ router.post('/login', async (req, res) => {
   }
 });
 
+router.put('/update/:email', async (req, res) => {
+  const { name, surname, email, password } = req.body;
+  const currentEmail = req.params.email;
+
+  try {
+    let user = await User.findOne({ email: currentEmail });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    user.name = name || user.name;
+    user.surname = surname || user.surname;
+    user.email = email || user.email;
+    
+    if (password) {
+      user.password = password;
+    }
+
+    await user.save();
+    res.status(200).json({ message: 'User updated successfully', user });
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
 module.exports = router;
