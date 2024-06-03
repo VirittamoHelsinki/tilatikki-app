9d356ddd7a85450d89d2549edf039e5d0c642e35
Roope Vuorenlehto <roope.vuorenlehto@edu.hel.fi>
2024-06-03 13:28:54 +0300

    Set filterValues in ReservationPage

--- client/src/pages/ReservationPage.jsx
+++ client/src/pages/ReservationPage.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import { Container, Box, TextField, Button, Typography } from '@mui/material';
 import Header from '../components/Header';
 import FilterForm from '../components/FilterForm';
 
 const ReservationPage = () => {
+	const [filterValues, setFilterValues] = useState({});
+
+	const handleFilterChange = (newFilterValues) => {
+		setFilterValues(newFilterValues);
+	}
+
   return (
     <>
     <Header/>
     <Container maxWidth="lg" style={{ display: 'flex', justifyContent: 'space-between', paddingTop: '20px' }}>
       <Box sx={{ width: '30%', padding: '20px', border: '1px solid #ddd', borderRadius: '4px' }}>
-		<FilterForm />
+		<FilterForm onFilterChange={handleFilterChange} />
       </Box>
 
       {/* Middle Section - Data Display */}
