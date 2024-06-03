67295148ba44cfbb336f9c8ca0a56a154b62a172
Joel Heusala <joel.heusala@gmail.com>
2024-06-03 22:49:09 +0300

    created schema plus react-query

--- client/src/pages/SchoolsPage.jsx
+++ client/src/pages/SchoolsPage.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import jatkasaariKoulu from '../images/jatkasaari-koulu.png';
 import pakilaKoulu from '../images/pakila-koulu.jpg';
+import { useSchoolQuery } from '../api/query';
 
 const cardData = [
   {
@@ -23,39 +24,45 @@ const cardData = [
 const SchoolsPage = () => {
   const navigate = useNavigate();
 
+  const { data: schools, isLoading, isError } = useSchoolQuery();
+
+  React.useEffect(() => console.log(
+    'schools: ', schools
+  ), [])
+
   const handleCardClick = (link) => {
     navigate(link);
   };
 
   return (
     <>
-    <Header/>
-    <Container sx={{ marginTop: 4 }}>
-      <Grid container spacing={4}>
-        {cardData.map((card, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
-            <Card>
-              <CardActionArea onClick={() => handleCardClick(card.link)}>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {card.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {card.description}
-                  </Typography>
-                  <CardMedia sx={{ marginTop: 4 }}
-                  component="img"
-                  height="140"
-                  image={card.image}
-                  alt={card.title}
-                />
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+      <Header />
+      <Container sx={{ marginTop: 4 }}>
+        <Grid container spacing={4}>
+          {cardData.map((card, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <Card>
+                <CardActionArea onClick={() => handleCardClick(card.link)}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {card.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {card.description}
+                    </Typography>
+                    <CardMedia sx={{ marginTop: 4 }}
+                      component="img"
+                      height="140"
+                      image={card.image}
+                      alt={card.title}
+                    />
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </>
   );
 };
