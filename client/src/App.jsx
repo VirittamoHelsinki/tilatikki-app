ca6b3a79be7f4f9301caead5498625e64a387bcd
Lagical <markuz.rika@gmail.com>
2024-06-03 12:20:25 +0300

    minor changes to routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,28 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import SchoolsPage from './pages/SchoolsPage';
 import ReservationPage from './pages/ReservationPage';
 import SettingsPage from './pages/SettingsPage';
 
-function App() {
 
+function App() {
   return (
     <>
-        <Router>
-          <div>
-            <Routes>
-                <Route path="/" element={<RegisterPage/>} />         
-                <Route path="/register" element={<RegisterPage/>} />
-                <Route path="/login" element={<LoginPage/>} />
-                <Route path="/schools" element={<SchoolsPage/>} />
-                <Route path="/reservations" element={<ReservationPage/>} />
-                <Route path="/settings" element={<SettingsPage/>} />
-            </Routes>
-            </div>
-        </Router>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/schools" element={<SchoolsPage />} />
+            <Route path="/reservations" element={<ReservationPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+          </Routes>
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
