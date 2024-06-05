2e776f156fc1338d5e4f00260b77d7b1d3f69806
Roope Vuorenlehto <roope.vuorenlehto@edu.hel.fi>
2024-06-05 16:30:50 +0300

    Add bookings to UserSchema

--- server/models/User.js
+++ server/models/User.js
@@ -2,27 +2,31 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  surname: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  admin: {
-    type: Boolean,
-    default: true
-  },
+	name: {
+		type: String,
+		required: true
+	},
+	surname: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	admin: {
+		type: Boolean,
+		default: false
+	},
+	bookings: [{
+		type: Schema.Types.ObjectId,
+		ref: 'Booking'
+	}]
 });
 
 UserSchema.methods.comparePassword = async function(candidatePassword) {
