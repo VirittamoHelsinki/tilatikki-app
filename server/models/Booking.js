872b91df4acfc7e5b06d12f3137bc3060679f437
Roope Vuorenlehto <roope.vuorenlehto@edu.hel.fi>
2024-06-06 15:09:26 +0300

    Fix times in BookingSchema

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -5,7 +5,11 @@ const BookingSchema = new mongoose.Schema({
 		type: String,
 		required: true
 	},
-	time: {
+	startTime: {
+		type: Date,
+		required: true
+	},
+	endTime: {
 		type: Date,
 		required: true
 	},
