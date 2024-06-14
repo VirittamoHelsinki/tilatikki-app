1478bacb29625bf8bf271c0e771f317b474fd581
Joel6677 <joel.heusala@gmail.com>
2024-06-14 11:04:55 +0300

    modifying card

--- client/src/components/BookingResults.jsx
+++ client/src/components/BookingResults.jsx
@@ -1,65 +1,3 @@
-// import React, { useState } from 'react';
-// import ReservationCard from './ReservationCard'; // Make sure this path is correct
-// import { Box, Button } from '@mui/material';
-//
-// const BookingResults = ({ classrooms }) => {
-// 	const [currentPage, setCurrentPage] = useState(0);
-// 	const itemsPerPage = 5;
-//
-// 	const handleNextPage = () => {
-// 		if (currentPage < Math.ceil(Object.keys(classrooms).length / itemsPerPage) - 1) {
-// 			setCurrentPage(currentPage + 1);
-// 		}
-// 	};
-//
-// 	const handlePreviousPage = () => {
-// 		if (currentPage > 0) {
-// 			setCurrentPage(currentPage - 1);
-// 		}
-// 	};
-//
-// 	const paginatedClassrooms = Object.entries(classrooms).slice(
-// 		currentPage * itemsPerPage,
-// 		(currentPage + 1) * itemsPerPage
-// 	);
-//
-// 	return (
-// 		<Box>
-// 			<h3>Huoneet</h3>
-// 			<Box sx={{ maxHeight: '500px', overflowY: 'auto' }}>
-// 				{paginatedClassrooms.map(([key, value]) =>
-// 					value.reservations.map((reservation) => (
-// 						<ReservationCard
-// 							key={reservation._id}
-// 							roomNumber={value.number}
-// 							purpose={reservation.purpose}
-// 							status="Varattu"  // Adjust the status logic as needed
-// 							capacity={value.capacity}
-// 							startTime={reservation.startTime}
-// 							endTime={reservation.endTime}
-// 						/>
-// 					))
-// 				)}
-// 			</Box>
-// 			<Box display="flex" justifyContent="space-between" mt={2}>
-// 				<Button onClick={handlePreviousPage} disabled={currentPage === 0}>
-// 					Edellinen
-// 				</Button>
-// 				<Button
-// 					onClick={handleNextPage}
-// 					disabled={currentPage >= Math.ceil(Object.keys(classrooms).length / itemsPerPage) - 1}
-// 				>
-// 					Seuraava
-// 				</Button>
-// 			</Box>
-// 		</Box>
-// 	);
-// };
-//
-// export default BookingResults;
-//
-
-
 import React, { useState } from 'react';
 import ReservationCard from './ReservationCard'; // Make sure this path is correct
 import { Box, Button } from '@mui/material';
@@ -95,6 +33,7 @@ const BookingResults = ({ classrooms }) => {
 							value.reservations.map((reservation) => (
 								<ReservationCard
 									key={reservation._id}
+									roomId={value._id}
 									roomNumber={value.number}
 									purpose={reservation.purpose}
 									status="Varattu"
--- client/src/components/ReservationCard.jsx
+++ client/src/components/ReservationCard.jsx
@@ -1,3 +1,86 @@
+// import React, { useState } from 'react';
+// import {
+// 	Card,
+// 	CardContent,
+// 	CardHeader,
+// 	Typography,
+// 	Box,
+// 	Chip,
+// 	Avatar,
+// 	Grid
+// } from '@mui/material';
+// import PeopleIcon from '@mui/icons-material/People';
+// import ReservationDialog from '../components/reservationDialog';
+//
+// const ReservationCard = ({ roomId, roomNumber, purpose, status, capacity, startTime, endTime, groupsize }) => {
+//
+// 	const [isOpen, setIsOpen] = useState(false);
+//
+// 	const handleOpenDialog = () => {
+// 		setIsOpen(true);
+// 	};
+//
+// 	const handleCloseDialog = () => {
+// 		setIsOpen(false);
+// 	};
+//
+// 	return (
+// 		<>
+// 			<Card variant="outlined">
+// 				<CardHeader
+// 					avatar={<Avatar aria-label="room-number">{roomNumber}</Avatar>}
+// 					title={
+// 						<Box display="flex" justifyContent="space-between" alignItems="center">
+// 							<Typography variant="h6">{`Room ${roomNumber}`}</Typography>
+//
+// 							<Box display="flex" justifyContent="flex-end" flexGrow={1} style={{ marginLeft: 30 }}>
+// 								{startTime && endTime && (
+// 									<Typography variant="body2" color="textSecondary">
+// 										{new Date(startTime).toLocaleString()} - {new Date(endTime).toLocaleString()}
+// 									</Typography>
+// 								)}
+// 							</Box>
+// 						</Box>
+// 					}
+// 					subheader={purpose}
+// 				/>
+// 				<CardContent>
+// 					<Grid container spacing={2}>
+// 						<Grid item xs={12} sm={6}>
+// 							<Box display="flex" alignItems="center">
+// 								<Chip
+// 									label={status}
+// 									color={status === 'Vapaa' ? 'success' : 'error'}
+// 									size="small"
+// 									sx={{ marginLeft: 1 }}
+// 								/>
+// 							</Box>
+// 						</Grid>
+// 						<Grid item xs={12} sm={6}>
+// 							<Box display="flex" alignItems="center">
+// 								<PeopleIcon sx={{ marginRight: 1 }} />
+// 								<Typography variant="body2" color="textSecondary">
+// 									{groupsize}/{capacity}
+// 								</Typography>
+// 							</Box>
+// 						</Grid>
+// 						<Grid item xs={12}>
+// 							<Button variant="outlined" onClick={handleOpenDialog}>
+// 								Open Reservation Dialog
+// 							</Button>
+// 						</Grid>
+// 					</Grid>
+// 				</CardContent>
+// 			</Card>
+// 			<ReservationDialog isOpen={isOpen} onClose={handleCloseDialog} roomId={roomId} />
+// 		</>
+// 	);
+// };
+//
+// export default ReservationCard;
+
+
+import React, { useState } from 'react';
 import {
 	Card,
 	CardContent,
@@ -6,51 +89,71 @@ import {
 	Box,
 	Chip,
 	Avatar,
-	Grid
+	Grid,
+	ButtonBase
 } from '@mui/material';
 import PeopleIcon from '@mui/icons-material/People';
+import ReservationDialog from '../components/reservationDialog';
 
-const ReservationCard = ({ roomNumber, purpose, status, capacity, startTime, endTime, groupsize }) => {
+const ReservationCard = ({ roomId, roomNumber, purpose, status, capacity, startTime, endTime, groupsize }) => {
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleOpenDialog = () => {
+		setIsOpen(true);
+	};
+
+	const handleCloseDialog = () => {
+		setIsOpen(false);
+	};
 
 	return (
-		<Card variant="outlined">
-			<CardHeader
-				avatar={<Avatar aria-label="room-number">{roomNumber}</Avatar>}
-				title={
-					<Box display="flex" justifyContent="space-between" alignItems="center">
-						<Typography variant="h6">{`Room ${roomNumber}`}</Typography>
-						<Box>
-							<Typography variant="body2" color="textSecondary">
-								{startTime ? new Date(startTime).toLocaleString() : ''}
-							</Typography>
-						</Box>
-					</Box>
-				}
-				subheader={purpose}
-			/>
-			<CardContent>
-				<Grid container spacing={2}>
-					<Grid item xs={12} sm={6}>
-						<Box display="flex" alignItems="center">
-							<Chip
-								label={status}
-								color={status === 'Vapaa' ? 'success' : 'error'}
-								size="small"
-								sx={{ marginLeft: 1 }}
-							/>
-						</Box>
-					</Grid>
-					<Grid item xs={12} sm={6}>
-						<Box display="flex" alignItems="center">
-							<PeopleIcon sx={{ marginRight: 1 }} />
-							<Typography variant="body2" color="textSecondary">
-								{groupsize}/{capacity}
-							</Typography>
-						</Box>
-					</Grid>
-				</Grid>
-			</CardContent>
-		</Card>
+		<>
+			<ButtonBase onClick={handleOpenDialog} sx={{ width: '100%', display: 'block', textAlign: 'left' }}>
+				<Card variant="outlined">
+					<CardHeader
+						avatar={<Avatar aria-label="room-number">{roomNumber}</Avatar>}
+						title={
+							<Box display="flex" justifyContent="space-between" alignItems="center">
+								<Typography variant="h6">{`Room ${roomNumber}`}</Typography>
+
+								<Box display="flex" justifyContent="flex-end" flexGrow={1} style={{ marginLeft: 30 }}>
+									{startTime && endTime && (
+										<Typography variant="body2" color="textSecondary">
+											{new Date(startTime).toLocaleString()} - {new Date(endTime).toLocaleString()}
+										</Typography>
+									)}
+								</Box>
+							</Box>
+						}
+						subheader={purpose}
+					/>
+					<CardContent>
+						<Grid container spacing={2}>
+							<Grid item xs={12} sm={6}>
+								<Box display="flex" alignItems="center">
+									<Chip
+										label={status}
+										color={status === 'Vapaa' ? 'success' : 'error'}
+										size="small"
+										sx={{ marginLeft: 1 }}
+									/>
+								</Box>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<Box display="flex" alignItems="center">
+									<PeopleIcon sx={{ marginRight: 1 }} />
+									<Typography variant="body2" color="textSecondary">
+										{groupsize}/{capacity}
+									</Typography>
+								</Box>
+							</Grid>
+						</Grid>
+					</CardContent>
+				</Card>
+			</ButtonBase>
+			<ReservationDialog isOpen={isOpen} onClose={handleCloseDialog} roomId={roomId} />
+		</>
 	);
 };
 
