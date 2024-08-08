aebe8f6fe2eb2a009a267819b40e9a490d8e7e9c
Roope Vuorenlehto <roope.vuorenlehto@edu.hel.fi>
2024-08-08 14:59:56 +0300

    Set next available timeslot for startingtime as default in Filter

--- client/src/components/FilterForm.jsx
+++ client/src/components/FilterForm.jsx
@@ -15,30 +15,6 @@ import {clearButtonStyle} from '../styles/filterformStyles';
 import InfoIcon from '@mui/icons-material/Info';
 import dayjs from 'dayjs';
 
-
-const FilterForm = ({ onClassroomChange, schoolData, onApply, onFilterChange }) => {
-	const [selectedBuildings, setSelectedBuildings] = useState([schoolData.buildings[0]]);
-	const [availableFloors, setAvailableFloors] = useState([1]);
-	const [selectedFloor, setSelectedFloor] = useState(schoolData.buildings[0].floors[0].number);
-	const [startingTime, setStartingTime] = useState('10:00');
-	const [endingTime, setEndingTime] = useState('11:00');
-	const [selectedGroupSize, setSelectedGroupSize] = useState(1);
-	const [classroom, setClassroom] = useState('');
-	const [availableClassrooms, setAvailableClassrooms] = useState([]);
-	const [selectedDate, setSelectedDate] = useState(dayjs());
-
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-	PaperProps: {
-		style: {
-			maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-			width: 250,
-		},
-	},
-};
-
 const timeSlots = [
 	'05:00', '05:15', '05:30', '05:45', '06:00', '06:15', '06:30', '06:45',
 	'07:00', '07:15', '07:30', '07:45', '08:00', '08:15', '08:30', '08:45',
@@ -49,8 +25,54 @@ const timeSlots = [
 	'17:00', '17:15', '17:30', '17:45', '18:00', '18:15', '18:30', '18:45',
 	'19:00', '19:15', '19:30', '19:45', '20:00', '20:15', '20:30', '20:45',
 	'21:00', '21:15', '21:30', '21:45', '22:00', '22:15', '22:30', '22:45',
-	'23:00', '23:15', '23:30'
+	'23:00', '23:15', '23:30', '23:45'
 ];
+
+const getNextAvailableStartingtime = () => {
+	const now = dayjs();
+
+	for (const time of timeSlots) {
+		const timeSlot = dayjs(time, 'HH:mm');
+		if (timeSlot.isAfter(now)) {
+		  return time;
+		}
+	  }
+	  return timeSlots[0];
+};
+
+const getEndingTime = (startingTime) => {
+	const startingIndex = timeSlots.indexOf(startingTime);
+	const endingIndex = startingIndex + 4;
+
+	if (endingIndex >= timeSlots.length) {
+		return '';
+	}
+	return timeSlots[endingIndex];
+};
+
+
+const FilterForm = ({ onClassroomChange, schoolData, onApply, onFilterChange }) => {
+	const [selectedBuildings, setSelectedBuildings] = useState([schoolData.buildings[0]]);
+	const [availableFloors, setAvailableFloors] = useState([1]);
+	const [selectedFloor, setSelectedFloor] = useState(schoolData.buildings[0].floors[0].number);
+	const [startingTime, setStartingTime] = useState(getNextAvailableStartingtime);
+	const [endingTime, setEndingTime] = useState(getEndingTime(getNextAvailableStartingtime));
+	const [selectedGroupSize, setSelectedGroupSize] = useState(1);
+	const [classroom, setClassroom] = useState('');
+	const [availableClassrooms, setAvailableClassrooms] = useState([]);
+	const [selectedDate, setSelectedDate] = useState(dayjs());
+
+
+	const ITEM_HEIGHT = 48;
+	const ITEM_PADDING_TOP = 8;
+	const MenuProps = {
+		PaperProps: {
+			style: {
+				maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+				width: 250,
+			},
+		},
+	};
 
 	// return the biggest classroom's capacity as maxvalue for groupsize
 	const maxClassroomCapacity = (schoolData) => {
@@ -383,6 +405,7 @@ const timeSlots = [
 			handleAvailableClassrooms();
 		}
 		setClassroom('');
+		setEndingTime(getEndingTime(startingTime));
 	}, [selectedBuildings, selectedFloor]);
 
 	return (
